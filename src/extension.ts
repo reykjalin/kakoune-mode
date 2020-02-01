5df63ec928fec91dad30d2dd5a0335fbc2dfa907
Kristófer Reykjalín Þorláksson <kristofer@thorlaksson.com>
2020-02-01 02:02:34 +0000

    Improve and fix linting

--- src/extension.ts
+++ src/extension.ts
@@ -2,29 +2,29 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-import { spawn, ChildProcess } from 'child_process';
+import { ChildProcess, spawn } from 'child_process';
 import { KeysMessage } from './rpc';
 
 import { handleCommand, showError } from './external/Extension.js';
 
 const startKakoune = (): ChildProcess => {
 	// Clear any previously used instances.
-	spawn('kak', ['-clear']);
+	spawn( 'kak', ['-clear'] );
 
 	const currentFile = vscode.window.activeTextEditor?.document.fileName || '';
-	const kak = spawn('kak', ['-ui', 'json', '-s', 'vscode', currentFile]);
-	kak.stdout.on('data', handleCommand);
-	kak.stderr.on('data', showError);
+	const kak = spawn( 'kak', ['-ui', 'json', '-s', 'vscode', currentFile] );
+	kak.stdout.on( 'data', handleCommand );
+	kak.stderr.on( 'data', showError );
 	return kak;
-}
+};
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export const activate = ( context: vscode.ExtensionContext ) => {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	if (vscode.window.activeTextEditor) {
+	if ( vscode.window.activeTextEditor ) {
 		vscode.window.activeTextEditor.options = { cursorStyle: vscode.TextEditorCursorStyle.Block };
 	}
 
@@ -32,49 +32,49 @@ export function activate(context: vscode.ExtensionContext) {
 	const kak = startKakoune();
 
 	// Override the typing command when in a text document.
-	overrideCommand(context, 'type', args => {
-		if (!args.text) { return; }
+	overrideCommand( context, 'type', args => {
+		if ( !args.text ) { return; }
 
-		const msg = new KeysMessage(args.text);
-		kak.stdin.write(JSON.stringify(msg));
-	});
+		const msg = new KeysMessage( args.text );
+		kak.stdin.write( JSON.stringify( msg ) );
+	} );
 
 	// Override the escape command.
-	const sendEscape = vscode.commands.registerCommand('extension.send_escape', () => {
-		const msg = new KeysMessage('<esc>');
+	const sendEscape = vscode.commands.registerCommand( 'extension.send_escape', () => {
+		const msg = new KeysMessage( '<esc>' );
 
-		kak.stdin.write(JSON.stringify(msg));
-	});
-	context.subscriptions.push(sendEscape);
+		kak.stdin.write( JSON.stringify( msg ) );
+	} );
+	context.subscriptions.push( sendEscape );
 
 	// Override the backspace command.
-	const sendBackspace = vscode.commands.registerCommand('extension.send_backspace', () => {
-		const msg = new KeysMessage('<backspace>');
+	const sendBackspace = vscode.commands.registerCommand( 'extension.send_backspace', () => {
+		const msg = new KeysMessage( '<backspace>' );
 
-		kak.stdin.write(JSON.stringify(msg));
-	});
-	context.subscriptions.push(sendBackspace);
+		kak.stdin.write( JSON.stringify( msg ) );
+	} );
+	context.subscriptions.push( sendBackspace );
 
 	// Make sure a documents are opened in kakoune.
-	vscode.window.onDidChangeActiveTextEditor(event => {
-		if (!event) {
+	vscode.window.onDidChangeActiveTextEditor( event => {
+		if ( !event ) {
 			return;
 		}
 
-		console.log(event.document.fileName);
+		console.log( event.document.fileName );
 
-		const msg = new KeysMessage(`:e ${event.document.fileName}<ret>`);
-		kak.stdin.write(JSON.stringify(msg));
-	});
-}
+		const msg = new KeysMessage( `:e ${event.document.fileName}<ret>` );
+		kak.stdin.write( JSON.stringify( msg ) );
+	} );
+};
 
-function overrideCommand(
+const overrideCommand = (
 	context: vscode.ExtensionContext,
 	command: string,
-	callback: (...args: any[]) => any
-) {
-	const disposable = vscode.commands.registerCommand(command, async args => {
-		if (!vscode.window.activeTextEditor) {
+	callback: ( ...args: any[] ) => any,
+) => {
+	const disposable = vscode.commands.registerCommand( command, async args => {
+		if ( !vscode.window.activeTextEditor ) {
 			return;
 		}
 
@@ -82,13 +82,13 @@ function overrideCommand(
 			vscode.window.activeTextEditor.document &&
 			vscode.window.activeTextEditor.document.uri.toString() === 'debug:input'
 		) {
-			return vscode.commands.executeCommand('default:' + command, args);
+			return vscode.commands.executeCommand( 'default:' + command, args );
 		}
 
-		return callback(args);
-	});
-	context.subscriptions.push(disposable);
-}
+		return callback( args );
+	} );
+	context.subscriptions.push( disposable );
+};
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export const deactivate = () => { };
